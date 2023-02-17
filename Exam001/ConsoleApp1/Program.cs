084f55b39ee906acbf45395787f3e7656dc76a5b
Darrel David <dddavid.works@gmail.com>
2023-02-17 21:02:16 +0800

    Initial commit

---
+++ Exam001/ConsoleApp1/Program.cs
@@ -0,0 +1,57 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        string testString = "{}";
+        TestMatchingBrackets(testString);
+
+        testString = "}{";
+        TestMatchingBrackets(testString);
+
+        testString = "{{}";
+        TestMatchingBrackets(testString);
+
+        testString = "";
+        TestMatchingBrackets(testString);
+
+        testString = "{abc...xyz}";
+        TestMatchingBrackets(testString);
+
+        testString = "{abc{xyz}";
+        TestMatchingBrackets(testString);
+
+        testString = "{abc{fgh}xyz}";
+        TestMatchingBrackets(testString);
+
+        Console.ReadLine();
+    }
+
+    static void TestMatchingBrackets(string input)
+    {
+        Console.WriteLine("Unit test for " + input + ": " + HasMatchingBrackets(input));
+    }
+
+    static bool HasMatchingBrackets(string input)
+    {
+        List<int> openings = new List<int>(), 
+            closings = new List<int>();
+
+        int inpLength = input.Length;
+        for (int i = 0; i < inpLength; i++)
+        {
+            if (input[i] == '{')
+            {
+                openings.Add(i);
+                for (int j = inpLength - 1; j > i; j--)
+                {
+                    char c2 = input[j];
+                    if (c2 == '}' && !closings.Any(cl => cl == j))
+                        closings.Add(j);
+                }
+            }
+        }
+
+        return openings.Count == closings.Count;
+    }
+
+}
